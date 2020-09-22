8fc245aa09881ca2fbc160d378a15ef115407814
Diego M. Rodríguez <diego.plan9@gmail.com>
2020-09-22 12:23:25 +0200

    Force encoding at open() calls (#13)
    Set explicitly the file encoding to `utf-8` in the `open()` calls, in
    order to prevent issues with different system locales.

--- setup.py
+++ setup.py
@@ -26,14 +26,14 @@ def get_version() -> str:
     """Get the package version."""
     version_path = os.path.join(
         os.path.dirname(__file__), 'src', 'aihwkit', 'VERSION.txt')
-    with open(version_path) as version_file:
+    with open(version_path, encoding='utf-8') as version_file:
         return version_file.read().strip()
 
 
 def get_long_description() -> str:
     """Get the package long description."""
     readme_path = os.path.join(os.path.dirname(__file__), 'README.md')
-    with open(readme_path) as readme_file:
+    with open(readme_path, encoding='utf-8') as readme_file:
         return readme_file.read().strip()
 
 
