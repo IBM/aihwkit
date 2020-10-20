49d6e5da2209725b4a1d8f911ed5a0b197673fbd
FABIO CARTA <carta@us.ibm.com>
2020-10-20 09:19:38 -0400

    CNN example with hardware aware behavior

---
+++ examples/6_lenet5_hardware_aware.py
@@ -0,0 +1,385 @@
+# -*- coding: utf-8 -*-
+
+# (C) Copyright 2020 IBM. All Rights Reserved.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""aihwkit example 6: analog CNN with hardware aware training.
+
+Mnist dataset on a LeNet5 inspired network.
+"""
+
+import os
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Imports from PyTorch.
+import torch
+import torch.nn.functional as F
+from torch import nn
+from torchvision import datasets, transforms
+
+# Imports from aihwkit.
+from aihwkit.nn import AnalogConv2d, AnalogLinear, AnalogSequential
+from aihwkit.optim.analog_sgd import AnalogSGD
+from aihwkit.simulator.configs import InferenceRPUConfig
+from aihwkit.simulator.configs.utils import OutputWeightNoiseType
+from aihwkit.simulator.noise_models import PCMLikeNoiseModel
+
+# Check device
+USE_CUDA = 0
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if torch.cuda.is_available():
+    USE_CUDA = 1
+
+# Path to store datasets
+TRAIN_DATASET = os.path.join('data', 'TRAIN_DATASET')
+TEST_DATASET = os.path.join('data', 'TEST_DATASET')
+
+# Path to store results
+RESULTS = os.path.join('results', 'LENET5')
+
+# Training parameters
+SEED = 1
+N_EPOCHS = 30
+BATCH_SIZE = 8
+LEARNING_RATE = 0.01
+N_CLASSES = 10
+
+# Define the properties of the neural network in terms of noise simulated during
+# the inference/training pass
+RPU_CONFIG = InferenceRPUConfig()
+RPU_CONFIG.forward.out_res = -1.  # Turn off (output) ADC discretization.
+RPU_CONFIG.forward.w_noise_type = OutputWeightNoiseType.ADDITIVE_CONSTANT
+RPU_CONFIG.forward.w_noise = 0.02
+RPU_CONFIG.noise_model = PCMLikeNoiseModel(g_max=25.0)
+
+
+def load_images():
+    """Load images for train from torchvision datasets."""
+    transform = transforms.Compose([transforms.ToTensor()])
+    train_set = datasets.MNIST(TRAIN_DATASET, download=True, train=True, transform=transform)
+    val_set = datasets.MNIST(TEST_DATASET, download=True, train=False, transform=transform)
+    train_data = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
+    validation_data = torch.utils.data.DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=False)
+
+    return train_data, validation_data
+
+
+class LeNet5(AnalogSequential):
+    """LeNet5 inspired analog model."""
+
+    def __init__(self):
+        super().__init__()
+
+        self.feature_extractor = nn.Sequential(
+            AnalogConv2d(in_channels=1, out_channels=16, kernel_size=5, stride=1,
+                         rpu_config=RPU_CONFIG),
+            nn.Tanh(),
+            nn.MaxPool2d(kernel_size=2),
+            AnalogConv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1,
+                         rpu_config=RPU_CONFIG),
+            nn.Tanh(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Tanh()
+        )
+
+        self.classifier = nn.Sequential(
+            AnalogLinear(in_features=512, out_features=128, rpu_config=RPU_CONFIG),
+            nn.Tanh(),
+            AnalogLinear(in_features=128, out_features=N_CLASSES, rpu_config=RPU_CONFIG),
+        )
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        x = torch.flatten(x, 1)
+        logits = self.classifier(x)
+        probs = F.softmax(logits, dim=1)
+
+        return probs
+
+
+def create_sgd_optimizer(model, learning_rate):
+    """Create the analog-aware optimizer.
+
+    Args:
+        model (nn.Module): model to be trained
+        learning_rate (float): global parameter to define learning rate
+    """
+    optimizer = AnalogSGD(model.parameters(), lr=learning_rate)
+    optimizer.regroup_param_groups(model)
+
+    return optimizer
+
+
+def train_step(train_data, model, criterion, optimizer):
+    """Train network.
+
+    Args:
+        train_data (DataLoader): Validation set to perform the evaluation
+        model (nn.Module): Trained model to be evaluated
+        criterion (nn.CrossEntropyLoss): criterion to compute loss
+        optimizer (Optimizer): analog model optimizer
+    """
+    total_loss = 0
+
+    model.train()
+
+    for images, labels in train_data:
+        images = images.to(DEVICE)
+        labels = labels.to(DEVICE)
+        optimizer.zero_grad()
+
+        # Add training Tensor to the model (input).
+        output = model(images)
+        loss = criterion(output, labels)
+
+        # Run training (backward propagation).
+        loss.backward()
+
+        # Optimize weights.
+        optimizer.step()
+        total_loss += loss.item() * images.size(0)
+    epoch_loss = total_loss / len(train_data.dataset)
+
+    return model, optimizer, epoch_loss
+
+
+def test_evaluation(validation_data, model, criterion):
+    """Test trained network
+
+    Args:
+        validation_data (DataLoader): Validation set to perform the evaluation
+        model (nn.Module): Trained model to be evaluated
+        criterion (nn.CrossEntropyLoss): criterion to compute loss
+    """
+    total_loss = 0
+    predicted_ok = 0
+    total_images = 0
+
+    model.eval()
+
+    for images, labels in validation_data:
+        images = images.to(DEVICE)
+        labels = labels.to(DEVICE)
+
+        pred = model(images)
+        loss = criterion(pred, labels)
+        total_loss += loss.item() * images.size(0)
+
+        _, predicted = torch.max(pred.data, 1)
+        total_images += labels.size(0)
+        predicted_ok += (predicted == labels).sum().item()
+        accuracy = predicted_ok/total_images*100
+        error = (1-predicted_ok/total_images)*100
+
+    epoch_loss = total_loss / len(validation_data.dataset)
+
+    return model, epoch_loss, error, accuracy
+
+
+def training_loop(model, criterion, optimizer, train_data, validation_data, epochs, print_every=1):
+    """Training loop.
+
+    Args:
+        model (nn.Module): Trained model to be evaluated
+        criterion (nn.CrossEntropyLoss): criterion to compute loss
+        optimizer (Optimizer): analog model optimizer
+        train_data (DataLoader): Validation set to perform the evaluation
+        validation_data (DataLoader): Validation set to perform the evaluation
+        epochs (int): global parameter to define epochs number
+        print_every (int): defines how many times to print training progress
+    """
+    train_losses = []
+    valid_losses = []
+    test_error = []
+
+    # Train model
+    for epoch in range(0, epochs):
+        # Train_step
+        model, optimizer, train_loss = train_step(train_data, model, criterion, optimizer)
+        train_losses.append(train_loss)
+
+        # Validate_step
+        with torch.no_grad():
+            model, valid_loss, error, accuracy = test_evaluation(
+                validation_data, model, criterion)
+            valid_losses.append(valid_loss)
+            test_error.append(error)
+
+        if epoch % print_every == (print_every - 1):
+            print(f'{datetime.now().time().replace(microsecond=0)} --- '
+                  f'Epoch: {epoch}\t'
+                  f'Train loss: {train_loss:.4f}\t'
+                  f'Valid loss: {valid_loss:.4f}\t'
+                  f'Test error: {error:.2f}%\t'
+                  f'Accuracy: {accuracy:.2f}%\t')
+
+    # Save results and plot figures
+    np.savetxt(os.path.join(RESULTS, "Test_error.csv"), test_error, delimiter=",")
+    np.savetxt(os.path.join(RESULTS, "Train_Losses.csv"), train_losses, delimiter=",")
+    np.savetxt(os.path.join(RESULTS, "Valid_Losses.csv"), valid_losses, delimiter=",")
+    plot_results(train_losses, valid_losses, test_error)
+
+    return model, optimizer, (train_losses, valid_losses, test_error)
+
+
+def plot_results(train_losses, valid_losses, test_error):
+    """Plot results.
+
+    Args:
+        train_losses: training losses as calculated in the training_loop
+        valid_losses: validation losses as calculated in the training_loop
+        test_error: test error as calculated in the training_loop
+    """
+    fig = plt.plot(train_losses, 'r-s', valid_losses, 'b-o')
+    plt.title('aihwkit LeNet5')
+    plt.legend(fig[:2], ['Training Losses', 'Validation Losses'])
+    plt.xlabel('Epoch number')
+    plt.ylabel('Loss [A.U.]')
+    plt.grid(which='both', linestyle='--')
+    plt.savefig(os.path.join(RESULTS, 'test_losses.png'))
+    plt.close()
+
+    fig = plt.plot(test_error, 'r-s')
+    plt.title('aihwkit LeNet5')
+    plt.legend(fig[:1], ['Validation Accuracy'])
+    plt.xlabel('Epoch number')
+    plt.ylabel('Test Error [%]')
+    plt.yscale('log')
+    plt.ylim((5e-1, 1e2))
+    plt.grid(which='both', linestyle='--')
+    plt.savefig(os.path.join(RESULTS, 'test_error.png'))
+    plt.close()
+
+
+def training_phase(model, criterion, optimizer, train_data, validation_data):
+    """Training phase.
+
+    Args:
+        model (nn.Module): Trained model to be evaluated
+        criterion (nn.CrossEntropyLoss): criterion to compute loss
+        optimizer (Optimizer): analog model optimizer
+        train_data (DataLoader): Validation set to perform the evaluation
+        validation_data (DataLoader): Validation set to perform the evaluation
+    """
+    print('\n ********************************************************* \n')
+    print(f'\n{datetime.now().time().replace(microsecond=0)} --- '
+          f'Started LeNet5 Training')
+
+    model, optimizer, _ = training_loop(model, criterion, optimizer, train_data, validation_data,
+                                        N_EPOCHS)
+
+    print(f'{datetime.now().time().replace(microsecond=0)} --- '
+          f'Completed LeNet5 Training')
+
+
+def inference_phase(model, criterion, optimizer, validation_data):
+    """Inference phase.
+
+    Args:
+        model (nn.Module): Trained model to be evaluated
+        criterion (nn.CrossEntropyLoss): criterion to compute loss
+        optimizer (Optimizer): analog model optimizer
+        validation_data (DataLoader): Validation set to perform the evaluation
+
+    """
+    total_loss = 0
+    predicted_ok = 0
+    total_images = 0
+    accuracy_pre = 0
+    error_pre = 0
+
+    model.eval()
+
+    print('\n ********************************************************* \n')
+    print(f'\n{datetime.now().time().replace(microsecond=0)} --- '
+          f'Started LeNet5 Inference')
+
+    # Accuracy after the training phase is completed.
+    for images, labels in validation_data:
+        images = images.to(DEVICE)
+        labels = labels.to(DEVICE)
+
+        pred = model(images)
+        loss = criterion(pred, labels)
+        total_loss += loss.item() * images.size(0)
+
+        _, predicted = torch.max(pred.data, 1)
+        total_images += labels.size(0)
+        predicted_ok += (predicted == labels).sum().item()
+        accuracy_pre = predicted_ok/total_images*100
+        error_pre = (1-predicted_ok/total_images)*100
+
+    print(f'{datetime.now().time().replace(microsecond=0)} --- '
+          f'Error after training: {error_pre:.2f}%\t'
+          f'Accuracy after training: {accuracy_pre:.2f}%\t')
+
+    # Simulation of inference pass at different times after training.
+    for t_inference in [0., 1., 20., 1000., 1e5]:
+        model.drift_analog_weights(t_inference)
+
+        time_since = t_inference
+        accuracy_post = 0
+        error_post = 0
+        predicted_ok = 0
+        total_images = 0
+
+        for images, labels in validation_data:
+            images = images.to(DEVICE)
+            labels = labels.to(DEVICE)
+
+            pred = model(images)
+            loss = criterion(pred, labels)
+            total_loss += loss.item() * images.size(0)
+
+            _, predicted = torch.max(pred.data, 1)
+            total_images += labels.size(0)
+            predicted_ok += (predicted == labels).sum().item()
+            accuracy_post = predicted_ok/total_images*100
+            error_post = (1-predicted_ok/total_images)*100
+
+        print(f'{datetime.now().time().replace(microsecond=0)} --- '
+              f'Error after inference: {error_post:.2f}%\t'
+              f'Accuracy after inference: {accuracy_post:.2f}%\t'
+              f'Drift t={time_since: .2e}\t')
+
+
+def main():
+    """Train a PyTorch CNN analog model with the MNIST dataset."""
+    # Make sure the directory where to save the results exist.
+    # Results include: Loss vs Epoch graph, Accuracy vs Epoch graph and vector data.
+    os.makedirs(RESULTS, exist_ok=True)
+    torch.manual_seed(SEED)
+
+    # Load datasets.
+    train_data, validation_data = load_images()
+
+    # Prepare the model.
+    model = LeNet5()
+    if USE_CUDA:
+        model.cuda()
+    print(model)
+
+    optimizer = create_sgd_optimizer(model, LEARNING_RATE)
+
+    criterion = nn.CrossEntropyLoss()
+
+    # Train the model
+    training_phase(model, criterion, optimizer, train_data, validation_data)
+
+    # Test model inference over time
+    inference_phase(model, criterion, optimizer, validation_data)
+
+
+if __name__ == '__main__':
+    # Execute only if run as the entry point into the program
+    main()
