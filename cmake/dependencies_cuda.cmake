ec9445a4547d1ff50945fada1230ab2ed3e47efe
Ivan Duran <ivan.duran.martinez@ibm.com>
2020-11-06 17:57:32 +0100

    Fix CUDA 11 compilation

--- cmake/dependencies_cuda.cmake
+++ cmake/dependencies_cuda.cmake
@@ -14,24 +14,29 @@ list(APPEND CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/cmake/Modules)
 if(USE_CUDA)
   enable_language(CUDA)
 
-  # ---[ CUB
-  find_package(CUB QUIET)
-  if(CUB_FOUND)
-    include_directories(SYSTEM ${CUB_INCLUDE_DIRS})
-  else()
-    include(ExternalProject)
-    ExternalProject_Add(cub
-      URL               https://github.com/NVlabs/cub/archive/1.8.0.zip
-      URL_HASH          MD5=a821b9dffbc9d1bacf1c8db2a59094bf
-      GIT_TAG        origin/release/1.2.3
-      CONFIGURE_COMMAND ""
-      BUILD_COMMAND ""
-      INSTALL_COMMAND ""
-    )
+  # --- [ CUDA Toolkit
+  find_package(CUDAToolkit)
 
-    ExternalProject_Get_Property(cub source_dir)
-    set(CUB_INCLUDE_DIR ${source_dir})
-    include_directories(SYSTEM ${CUB_INCLUDE_DIR})
+  # ---[ CUB (Only for CUDA v. less than 11)
+  if(${CUDAToolkit_VERSION_MAJOR} LESS 11)
+    find_package(CUB QUIET)
+    if(CUB_FOUND)
+      include_directories(SYSTEM ${CUB_INCLUDE_DIRS})
+    else()
+      include(ExternalProject)
+      ExternalProject_Add(cub
+        URL               https://github.com/NVlabs/cub/archive/1.8.0.zip
+        URL_HASH          MD5=a821b9dffbc9d1bacf1c8db2a59094bf
+        GIT_TAG        origin/release/1.2.3
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ""
+        INSTALL_COMMAND ""
+      )
+
+      ExternalProject_Get_Property(cub source_dir)
+      set(CUB_INCLUDE_DIR ${source_dir})
+      include_directories(SYSTEM ${CUB_INCLUDE_DIR})
+    endif(CUB_FOUND)
   endif()
 
   include_directories(SYSTEM ${CMAKE_CUDA_TOOLKIT_INCLUDE_DIRECTORIES})
