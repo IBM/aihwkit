a6a326b8ca8d9ece8ff637463d89482b6b86cce7
Diego M. Rodriguez <diego.plan9@gmail.com>
2020-11-25 16:28:26 +0100

    Fix typo

--- src/aihwkit/simulator/configs/helpers.py
+++ src/aihwkit/simulator/configs/helpers.py
@@ -48,8 +48,8 @@ def tile_parameters_to_bindings(params: Any) -> Any:
     """Convert a tile dataclass parameter into a bindings class."""
     field_map = {'forward': 'forward_io',
                  'backward': 'backward_io'}
-    excuded_fields = ('device', 'noise_model', 'drift_compensation',
-                      'clip', 'modifier')
+    excluded_fields = ('device', 'noise_model', 'drift_compensation',
+                       'clip', 'modifier')
 
     result = params.bindings_class()
     for field, value in params.__dict__.items():
@@ -57,7 +57,7 @@ def tile_parameters_to_bindings(params: Any) -> Any:
         field = field_map.get(field, field)
 
         # Convert enums to the bindings enums.
-        if field in excuded_fields:
+        if field in excluded_fields:
             # Exclude special fields that are not present in the bindings.
             continue
 
