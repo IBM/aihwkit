362d020f627d503e5242afa4434326f7fff78122
Diego M. Rodriguez <diego.plan9@gmail.com>
2021-06-23 09:11:36 +0200

    Update mixed precision

--- src/rpucuda/utility_functions.h
+++ src/rpucuda/utility_functions.h
@@ -83,7 +83,7 @@
 
 namespace RPU {
 
-template <typename T, typename... Args> std::unique_ptr<T> make_unique(Args &&...args) {
+template <typename T, typename... Args> std::unique_ptr<T> make_unique(Args &&... args) {
   return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
