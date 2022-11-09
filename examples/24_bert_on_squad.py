5d651a18e571ec0a62bdffb4b246af23ebddc628
Henry Ye <yehenry11@gmail.com>
2022-11-08 21:12:01 -0500

    Added AnalogSGD optimizer and custom RPUConfig
    Signed-off-by: Henry Ye <yehenry11@gmail.com>

--- examples/24_bert_on_squad.py
+++ examples/24_bert_on_squad.py
@@ -15,7 +15,10 @@
 # pylint: disable=invalid-name
 # pylint: disable=too-many-locals
 
-from aihwkit.simulator.configs import SingleRPUConfig
+from aihwkit.simulator.configs import InferenceRPUConfig
+from aihwkit.simulator.configs.utils import WeightClipType, WeightNoiseType, BoundManagementType
+from aihwkit.simulator.noise_models import PCMLikeNoiseModel, GlobalDriftCompensation
+from aihwkit.simulator.presets.utils import PresetIOParameters
 from aihwkit.nn.conversion import convert_to_analog
 from aihwkit.optim import AnalogSGD
 
@@ -24,14 +27,31 @@ from transformers import (AutoTokenizer, BertForQuestionAnswering,
 
 from datasets import load_dataset
 
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+def create_rpu_config(g_max = 160, tile_size=256, dac_res=256, adc_res=256, w_noise=0.1):
+    rpu_config = InferenceRPUConfig()
+    rpu_config.clip.type = WeightClipType.FIXED_VALUE
+    rpu_config.clip.fixed_value = 1.0
+    rpu_config.modifier.rel_to_actual_wmax = True
+    rpu_config.mapping.digital_bias = True
+    rpu_config.mapping.learn_out_scaling_alpha = True
+    rpu_config.mapping.weight_scaling_omega = 0.4
+    rpu_config.mapping.weight_scaling_omega_columnwise = True
+    rpu_config.mapping.max_input_size = tile_size
+    rpu_config.mapping.max_output_size = 255
+    rpu_config.forward = PresetIOParameters()
+    rpu_config.forward.w_noise_type = WeightNoiseType.ADDITIVE_CONSTANT
+    rpu_config.forward.w_noise = w_noise
+    rpu_config.forward.inp_res = 1/dac_res
+    rpu_config.forward.out_res = 1/adc_res
+    rpu_config.forward.bound_management = BoundManagementType.NONE
+    rpu_config.noise_model = PCMLikeNoiseModel(g_max=g_max)
+    rpu_config.drift_compensation = GlobalDriftCompensation()
+    return rpu_config
 
-model = BertForQuestionAnswering.from_pretrained("bert-base-uncased")
-model = convert_to_analog(model, SingleRPUConfig())
-
-squad = load_dataset("squad")
-
-print(squad["train"][0])
+def create_model(rpu_config):
+    model = BertForQuestionAnswering.from_pretrained("bert-base-uncased")
+    model = convert_to_analog(model, rpu_config)
+    return model
 
 # From huggingface tutorial on question answering
 # Some examples in the dataset may have contexts that exceed the maximum input length
@@ -101,43 +121,62 @@ def preprocess(dataset):
     inputs["end_positions"] = end_positions
     return inputs
 
-# Preprocessing changes number of samples, so we need to remove some columns so
-# the data updates properly
-squad = squad.map(preprocess, batched=True, remove_columns=squad["train"].column_names)
+def create_dataset():
+    squad = load_dataset("squad")
 
-training_args = TrainingArguments(
-    output_dir='./',
-    evaluation_strategy="epoch",
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    num_train_epochs=3,
-    weight_decay=0.01,
-)
+    print(squad["train"][0])
 
-collator = DefaultDataCollator()
+    # Preprocessing changes number of samples, so we need to remove some columns so
+    # the data updates properly
+    squad = squad.map(preprocess, batched=True, remove_columns=squad["train"].column_names)
+    return squad
 
-optimizer = AnalogSGD(model.parameters(), lr=2e-4)
+def create_optimizer(model):
+    optimizer = AnalogSGD(model.parameters(), lr=2e-4)
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    data_collator=collator,
-    train_dataset=squad["train"],
-    eval_dataset=squad["validation"],
-    tokenizer=tokenizer,
-    optimizers=(optimizer, None)
-)
+    optimizer.regroup_param_groups(model)
+    return optimizer
 
-trainer.train()
+def fine_tune(model, optimizer, squad, tokenizer):
+    training_args = TrainingArguments(
+        output_dir='./',
+        evaluation_strategy="epoch",
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        num_train_epochs=3,
+        weight_decay=0.01,
+    )
+
+    collator = DefaultDataCollator()
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        data_collator=collator,
+        train_dataset=squad["train"],
+        eval_dataset=squad["validation"],
+        tokenizer=tokenizer,
+        optimizers=(optimizer, None)
+    )
+
+    trainer.train()
+
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+
+rpu_config = create_rpu_config()
+model = create_model(rpu_config)
+squad = create_dataset()
+optimizer = create_optimizer(model)
+
+fine_tune(model, optimizer, squad, tokenizer)
 
 ''' Next steps
-        - Create analog optimizer
+        - Convert to notebook
         - Start using tensor board to track info + debugging
-        - Use a custom RPU configuration for Inference on PCM device
-
-    Drift Experiment
-        - Take digital model and fine-tune it on a task + use it for inference
-        - Perform hardware-aware training with same model
-        - Show that hwa training creates a more robust model
-            since drift is accomodated for over time
+        - Drift Experiment
+            - Take digital model and fine-tune it on a task + use it for inference
+            - Perform hardware-aware training with model
+            - Show that hwa training creates a more robust model
+                since drift is accomodated for over time
 '''
