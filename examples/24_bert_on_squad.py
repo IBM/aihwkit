aaae5ac76f68dbcfaadb9963878a859b05921f58
Henry Ye <yehenry11@gmail.com>
2022-11-09 19:26:03 -0500

    Validation preprocessing and loading bert on squad
    Signed-off-by: Henry Ye <yehenry11@gmail.com>

--- examples/24_bert_on_squad.py
+++ examples/24_bert_on_squad.py
@@ -22,10 +22,10 @@ from aihwkit.simulator.presets.utils import PresetIOParameters
 from aihwkit.nn.conversion import convert_to_analog
 from aihwkit.optim import AnalogSGD
 
-from transformers import (AutoTokenizer, BertForQuestionAnswering,
+from transformers import (AutoTokenizer, AutoModelForQuestionAnswering,
                         Trainer, TrainingArguments, DefaultDataCollator)
 
-from datasets import load_dataset
+from datasets import load_dataset, load_metric
 
 def create_rpu_config(g_max = 160, tile_size=256, dac_res=256, adc_res=256, w_noise=0.1):
     rpu_config = InferenceRPUConfig()
@@ -48,16 +48,17 @@ def create_rpu_config(g_max = 160, tile_size=256, dac_res=256, adc_res=256, w_no
     rpu_config.drift_compensation = GlobalDriftCompensation()
     return rpu_config
 
-def create_model(rpu_config):
-    model = BertForQuestionAnswering.from_pretrained("bert-base-uncased")
+def create_model_and_tokenizer(rpu_config):
+    model = AutoModelForQuestionAnswering.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
     model = convert_to_analog(model, rpu_config)
-    return model
+
+    tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
+    return model, tokenizer
 
 # From huggingface tutorial on question answering
 # Some examples in the dataset may have contexts that exceed the maximum input length
 #   We can truncate the context using truncation="only_second"
-#
-def preprocess(dataset):
+def preprocess_train(dataset, tokenizer):
     questions = [ q.strip() for q in dataset["question"] ]
     inputs = tokenizer(
         questions,
@@ -121,6 +122,51 @@ def preprocess(dataset):
     inputs["end_positions"] = end_positions
     return inputs
 
+def preprocess_validation(dataset, tokenizer):
+    # Some of the questions have lots of whitespace on the left, which is not useful and will make the
+    # truncation of the context fail (the tokenized question will take a lots of space). So we remove that
+    # left whitespace
+    dataset["question"] = [ q.lstrip() for q in dataset["question"] ]
+
+    # Tokenize our dataset with truncation and maybe padding, but keep the overflows using a stride. This results
+    # in one example possible giving several features when a context is long, each of those features having a
+    # context that overlaps a bit the context of the previous feature.
+    tokenized_dataset = tokenizer(
+        dataset["question"],
+        dataset["context"],
+        truncation="only_second",
+        max_length=384,
+        stride=128,
+        return_overflowing_tokens=True,
+        return_offsets_mapping=True,
+        padding="max_length",
+    )
+
+    # Since one example might give us several features if it has a long context, we need a map from a feature to
+    # its corresponding example. This key gives us just that.
+    sample_mapping = tokenized_dataset.pop("overflow_to_sample_mapping")
+
+    # We keep the example_id that gave us this feature and we will store the offset mappings.
+    tokenized_dataset["example_id"] = []
+
+    for i in range(len(tokenized_dataset["input_ids"])):
+        # Grab the sequence corresponding to that example (to know what is the context and what is the question).
+        sequence_ids = tokenized_dataset.sequence_ids(i)
+        context_index = 1
+
+        # One example can give several spans, this is the index of the example containing this span of text.
+        sample_index = sample_mapping[i]
+        tokenized_dataset["example_id"].append(dataset["id"][sample_index])
+
+        # Set to None the offset_mapping that are not part of the context so it's easy to determine if a token
+        # position is part of the context or not.
+        tokenized_dataset["offset_mapping"][i] = [
+            (o if sequence_ids[k] == context_index else None)
+            for k, o in enumerate(tokenized_dataset["offset_mapping"][i])
+        ]
+
+    return tokenized_dataset
+
 def create_dataset():
     squad = load_dataset("squad")
 
@@ -128,7 +174,7 @@ def create_dataset():
 
     # Preprocessing changes number of samples, so we need to remove some columns so
     # the data updates properly
-    squad = squad.map(preprocess, batched=True, remove_columns=squad["train"].column_names)
+    squad = squad.map(preprocess_train, batched=True, remove_columns=squad["train"].column_names)
     return squad
 
 def create_optimizer(model):
@@ -137,10 +183,10 @@ def create_optimizer(model):
     optimizer.regroup_param_groups(model)
     return optimizer
 
-def fine_tune(model, optimizer, squad, tokenizer):
+def make_trainer(model, optimizer, squad, tokenizer):
     training_args = TrainingArguments(
         output_dir='./',
-        evaluation_strategy="epoch",
+        evaluation_strategy="no",
         per_device_train_batch_size=16,
         per_device_eval_batch_size=16,
         num_train_epochs=3,
@@ -159,17 +205,26 @@ def fine_tune(model, optimizer, squad, tokenizer):
         optimizers=(optimizer, None)
     )
 
-    trainer.train()
+    return trainer
 
+def do_inference(model, trainer, squad, t_inferences=[0., 1., 20., 1000., 1e5]):
+    eval_data = squad["validation"].map(preprocess_validation, batched=True, remove_columns=squad["validation"].column_names)
 
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+    raw_predictions = trainer.predict(eval_data)
+
+    # TODO: create function to postprocess predictions and evaluate the
+
+    for t_inference in t_inferences:
+        model.drift_analog_weights(t_inference)
+
+        # Perform inference + evaluate metric here
+
 
 rpu_config = create_rpu_config()
-model = create_model(rpu_config)
+model, tokenizer = create_model_and_tokenizer(rpu_config)
 squad = create_dataset()
 optimizer = create_optimizer(model)
 
-fine_tune(model, optimizer, squad, tokenizer)
 
 ''' Next steps
         - Convert to notebook
