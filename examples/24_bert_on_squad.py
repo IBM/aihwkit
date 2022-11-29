0fed1011e391b1d9bcd7dd57e678174a8dffd779
Henry Ye <yehenry11@gmail.com>
2022-11-29 18:11:23 -0500

    Changed RPU parameters
    Signed-off-by: Henry Ye <yehenry11@gmail.com>

--- examples/24_bert_on_squad.py
+++ examples/24_bert_on_squad.py
@@ -47,23 +47,23 @@ from datasets import load_dataset
 max_length=320
 doc_stride=128
 
-def create_rpu_config(g_max=160, tile_size=256, dac_res=256, adc_res=256, w_noise=0.1):
+def create_rpu_config(g_max=160, tile_size=256, dac_res=256, adc_res=256, w_noise=0.0175):
     rpu_config = InferenceRPUConfig()
     rpu_config.clip.type = WeightClipType.FIXED_VALUE
     rpu_config.clip.fixed_value = 1.0
     rpu_config.modifier.rel_to_actual_wmax = True
     rpu_config.mapping.digital_bias = True
     rpu_config.mapping.learn_out_scaling_alpha = True
-    rpu_config.mapping.weight_scaling_omega = 0.4
+    rpu_config.mapping.weight_scaling_omega = 1
     rpu_config.mapping.weight_scaling_omega_columnwise = True
     rpu_config.mapping.max_input_size = tile_size
     rpu_config.mapping.max_output_size = 255
     rpu_config.forward = PresetIOParameters()
-    rpu_config.forward.w_noise_type = WeightNoiseType.ADDITIVE_CONSTANT
+    rpu_config.forward.w_noise_type = WeightNoiseType.PCM_READ
     rpu_config.forward.w_noise = w_noise
     rpu_config.forward.inp_res = 1/dac_res
     rpu_config.forward.out_res = 1/adc_res
-    rpu_config.forward.bound_management = BoundManagementType.NONE
+    rpu_config.forward.bound_management = BoundManagementType.ITERATIVE
     rpu_config.noise_model = PCMLikeNoiseModel(g_max=g_max)
     rpu_config.drift_compensation = GlobalDriftCompensation()
     return rpu_config
@@ -377,6 +377,7 @@ do_inference(model, trainer, squad, eval_data, writer)
             - Perform hardware-aware training with model
             - Show that hwa training creates a more robust model
                 since drift is accomodated for over time
+            - Integrate wandb for hypterparameter tuning
         - Add setup/installation steps needed to run example to README
         - Distributed compute
         - Convert to notebook
