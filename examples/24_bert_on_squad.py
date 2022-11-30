412d60e01d61fc751cf1bfd2de80e357641bbf90
Henry Ye <yehenry11@gmail.com>
2022-11-30 18:50:19 -0500

    Integrated wandb
    Signed-off-by: Henry Ye <yehenry11@gmail.com>

--- examples/24_bert_on_squad.py
+++ examples/24_bert_on_squad.py
@@ -45,20 +45,46 @@ from torch.utils.tensorboard import SummaryWriter
 from evaluate import load
 from datasets import load_dataset
 
-# Parse some arguments
-parser = argparse.ArgumentParser('Analog BERT on SQuAD example')
-parser.add_argument('-d', '--digital',
-                    help='Add to use digital inference',
-                    action='store_true')
-parser.add_argument('-i', '--ideal',
-                    help='Add to use ideal config instead of default noisy one',
-                    action='store_true')
 
-args = parser.parse_args()
+MODEL_NAME = "csarron/bert-base-uncased-squad-v1"
+TOKENIZER = AutoTokenizer.from_pretrained(MODEL_NAME)
+
+# Parse some arguments
+PARSER = argparse.ArgumentParser("Analog BERT on SQuAD example")
+PARSER.add_argument("-d", "--digital",
+                    help="Add to use digital inference",
+                    action="store_true")
+PARSER.add_argument("-i", "--ideal",
+                    help="Add to use ideal config instead of default noisy one",
+                    action="store_true")
+PARSER.add_argument("-w", "--wandb",
+                    help="Add to use wandb",
+                    action="store_true")
+
+ARGS = PARSER.parse_args()
+
+if ARGS.wandb:
+    import wandb
+
+    # Define weights noise sweep configuration
+    SWEEP_CONFIG = {
+        "method": "random",
+        "name": "weight noise sweep",
+        "metric": {
+            "goal": "maximize",
+            "name": "exact_match"
+        },
+        "parameters": {
+            "weight_noise": { "values": [ 0, 0.00875, 0.0175, 0.035, 0.07 ] }
+        }
+    }
+
+    SWEEP_ID = wandb.sweep(sweep=SWEEP_CONFIG, project="bert-weight-noise-experiment")
 
 # max length and stride specific to pretrained model
-max_length=320
-doc_stride=128
+MAX_LENGTH=320
+DOC_STRIDE=128
+
 
 def create_ideal_rpu_config(g_max=160, tile_size=256, w_noise=0.0, out_noise=0.0):
     rpu_config = InferenceRPUConfig()
@@ -84,6 +110,9 @@ def create_ideal_rpu_config(g_max=160, tile_size=256, w_noise=0.0, out_noise=0.0
     return rpu_config
 
 def create_rpu_config(g_max=160, tile_size=256, dac_res=256, adc_res=256, w_noise=0.0175):
+    if ARGS.wandb:
+        w_noise = wandb.config.weight_noise
+
     rpu_config = InferenceRPUConfig()
     rpu_config.clip.type = WeightClipType.FIXED_VALUE
     rpu_config.clip.fixed_value = 1.0
@@ -104,17 +133,13 @@ def create_rpu_config(g_max=160, tile_size=256, dac_res=256, adc_res=256, w_nois
     rpu_config.drift_compensation = GlobalDriftCompensation()
     return rpu_config
 
-def create_model_and_tokenizer(rpu_config):
-    model = AutoModelForQuestionAnswering.from_pretrained("csarron/bert-base-uncased-squad-v1")
-    if not args.digital:
+def create_model(rpu_config):
+    model = AutoModelForQuestionAnswering.from_pretrained(MODEL_NAME)
+    if not ARGS.digital:
         model = AnalogSequential(convert_to_analog_mapped(model, rpu_config))
     print(model)
 
-    tokenizer = AutoTokenizer.from_pretrained("csarron/bert-base-uncased-squad-v1")
-    return model, tokenizer
-
-rpu_config = create_ideal_rpu_config() if args.ideal else create_rpu_config()
-model, tokenizer = create_model_and_tokenizer(rpu_config)
+    return model
 
 # Some examples in the dataset may have contexts that exceed the maximum input length
 # We can truncate the context using truncation="only_second"
@@ -128,12 +153,12 @@ def preprocess_train(dataset):
     # in one example possibly giving several features when a context is long, each of those features having a
     # context that overlaps a bit the context of the previous feature, the stride being the number
     # of overlapping tokens in the overlap.
-    tokenized_dataset = tokenizer(
+    tokenized_dataset = TOKENIZER(
         dataset["question"],
         dataset["context"],
         truncation="only_second",
-        max_length=max_length,
-        stride=doc_stride,
+        max_length=MAX_LENGTH,
+        stride=DOC_STRIDE,
         return_overflowing_tokens=True,
         return_offsets_mapping=True,
         padding="max_length",
@@ -153,7 +178,7 @@ def preprocess_train(dataset):
     for i, offsets in enumerate(offset_mapping):
         # We will label impossible answers with the index of the CLS token.
         input_ids = tokenized_dataset["input_ids"][i]
-        cls_index = input_ids.index(tokenizer.cls_token_id)
+        cls_index = input_ids.index(TOKENIZER.cls_token_id)
 
         # Grab the sequence corresponding to that example (to know what is the context and what is the question).
         sequence_ids = tokenized_dataset.sequence_ids(i)
@@ -205,12 +230,12 @@ def preprocess_validation(dataset):
     # Tokenize our dataset with truncation and maybe padding, but keep the overflows using a stride. This results
     # in one example possible giving several features when a context is long, each of those features having a
     # context that overlaps a bit the context of the previous feature.
-    tokenized_dataset = tokenizer(
+    tokenized_dataset = TOKENIZER(
         dataset["question"],
         dataset["context"],
         truncation="only_second",
-        max_length=max_length,
-        stride=doc_stride,
+        max_length=MAX_LENGTH,
+        stride=DOC_STRIDE,
         return_overflowing_tokens=True,
         return_offsets_mapping=True,
         padding="max_length",
@@ -344,7 +369,7 @@ def create_optimizer(model):
 
     return optimizer
 
-def make_trainer(model, optimizer, tokenized_data, tokenizer):
+def make_trainer(model, optimizer, tokenized_data):
     training_args = TrainingArguments(
         output_dir='./',
         save_strategy="no",
@@ -360,12 +385,12 @@ def make_trainer(model, optimizer, tokenized_data, tokenizer):
     writer = SummaryWriter(log_dir=log_dir)
 
     trainer = Trainer(
-        model=model if args.digital else model[0],
+        model=model if ARGS.digital else model[0],
         args=training_args,
         data_collator=collator,
         train_dataset=tokenized_data["train"],
         eval_dataset=tokenized_data["validation"],
-        tokenizer=tokenizer,
+        tokenizer=TOKENIZER,
         optimizers=(optimizer, None),
         callbacks=[ TensorBoardCallback(writer) ]
     )
@@ -394,32 +419,48 @@ def do_inference(model, trainer, squad, eval_data, writer, max_inference_time=1e
         out_metric = metric.compute(predictions=formatted_preds, references=ground_truth)
         f1, exact_match = out_metric["f1"], out_metric["exact_match"]
 
-        writer.add_scalar('val/f1', f1, t_inference)
-        writer.add_scalar('val/exact_match', exact_match, t_inference)
+        writer.add_scalar("val/f1", f1, t_inference)
+        writer.add_scalar("val/exact_match", exact_match, t_inference)
+
+        if ARGS.wandb:
+            wandb.log({
+                "t_inference": t_inference,
+                "f1": f1,
+                "exact_match": exact_match,
+            })
 
         exact_match, f1, drift = exact_match, f1, t_inference
         print(f"Exact match: {exact_match: .2f}\t"
               f"F1: {f1: .2f}\t"
               f"Drift: {drift: .2e}")
 
-squad, tokenized_data, eval_data = create_datasets()
-optimizer = create_optimizer(model)
-trainer, writer = make_trainer(model, optimizer, tokenized_data, tokenizer)
+def main():
+    if ARGS.wandb:
+        wandb.init()
 
-# Do hw-aware training
-# trainer.train()
+    # Define RPU configuration and use it to create model and tokenizer
+    rpu_config = create_ideal_rpu_config() if ARGS.ideal else create_rpu_config()
+    model = create_model(rpu_config)
+    squad, tokenized_data, eval_data = create_datasets()
+    optimizer = create_optimizer(model)
+    trainer, writer = make_trainer(model, optimizer, tokenized_data)
 
-do_inference(model, trainer, squad, eval_data, writer)
+    # Do hw-aware training
+    # trainer.train()
 
+    do_inference(model, trainer, squad, eval_data, writer)
 
-''' Next steps
+if ARGS.wandb:
+    wandb.agent(SWEEP_ID, function=main, count=4)
+else:
+    main()
+
+""" Next steps
         - Drift Experiment
-            - Take digital model and fine-tune it on a task + use it for inference
-            - Perform hardware-aware training with model
-            - Show that hwa training creates a more robust model
-                since drift is accomodated for over time
-            - Integrate wandb for hypterparameter tuning
-        - Add setup/installation steps needed to run example to README
+            - Integrate wandb for weight noise experiments
         - Distributed compute
+        - Add setup/installation steps needed to run example to README
+        - Show that hwa training creates a more robust model
+            since drift is accomodated for over time
         - Convert to notebook
-'''
+"""
