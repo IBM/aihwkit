fa7b7f6d0397c94ea6405c8128c847e2bffa5eb0
kkvtran <kvtran@us.ibm.com>
2024-01-22 17:15:18 -0600

    add analog_fusion notebook (#601)
    * add analog_fusion notebook
    
    * Fixing the images links
    
    * update documentation
    
    ---------
    
    Co-authored-by: KAOUTAR EL MAGHRAOUI <kelmaghr@neha.sl.cloud9.ibm.com>

--- src/aihwkit/simulator/parameters/io.py
+++ src/aihwkit/simulator/parameters/io.py
@@ -178,7 +178,7 @@ class IOParameters(_PrintableMixin):
     analog output (before the ADC), i.e. :math:`\frac{y_i}{1 +
     n_i*|y_i|}` where :math:`n_i` is drawn at the instantiation time
     by::
-        out_nonlinearity / out_bound * (1 + out_nonlinearity_std * rand)
+    out_nonlinearity / out_bound * (1 + out_nonlinearity_std * rand)
     """
 
     out_nonlinearity_std: float = 0.0
