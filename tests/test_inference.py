00424786ccbfb1d5807b580b4a7290e3318053fb
pablocarmona <pablocarmonagonzalez@gmail.com>
2024-09-18 19:10:41 +0200

    add release content for 0.9.2 (#685)
    * add release content for 0.9.2
    
    Signed-off-by: Pablo Carmona <pablocarmonagonzalez@gmail.com>
    
    * remove added patch text
    
    Signed-off-by: Pablo Carmona <pablocarmonagonzalez@gmail.com>
    
    * modify changelog.md and version
    
    Signed-off-by: Pablo Carmona <pablocarmonagonzalez@gmail.com>
    
    * ufix(examples/31_custom_drift_models.py): change way rpu config is setup to a cleaner way
    
    Signed-off-by: Pablo Carmona <pablocarmonagonzalez@gmail.com>
    
    * fix(examples/31_custom_drift_models.py): fix errors with pycodestyle
    
    Signed-off-by: Pablo Carmona <pablocarmonagonzalez@gmail.com>
    
    * feat(notebooks): clean up and enhacenments on notebook contents for clarification and typos fixings
    
    * feat(converter/conductance): new conductance converters development
    
    * updates to custom drift model suggested by Malte (#690)
    
    Co-authored-by: Charles Mackin <charles.mackin@ibm.com>
    
    * Update hermes.py (#691)
    
    * fix(hermes.py): fix linting errors related to whitespacing
    
    * fix(requirements.txt): solve problem with numpy versions in some cases
    
    * feat(CHANGELOG.md): add PR number for new related developments and modify the date of release to a proper one
    
    * feat(CHANGELOG.md): add PR number for new related developments and modify the date of release to a proper one
    
    * feat(travis): update travis build with latest pytorch version
    
    ---------
    
    Signed-off-by: Pablo Carmona <pablocarmonagonzalez@gmail.com>
    Co-authored-by: charlesmackin <45808803+charlesmackin@users.noreply.github.com>
    Co-authored-by: Charles Mackin <charles.mackin@ibm.com>
    Co-authored-by: Athanasios Vasilopoulos <152185220+atvasilopoulos@users.noreply.github.com>
    Signed-off-by: Pablo Carmona González <pxg@Pablos-MacBook-Pro.local>

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -16,8 +16,12 @@ from torch import randn
 
 from aihwkit.inference import (
     PCMLikeNoiseModel,
+    CustomDriftPCMLikeNoiseModel,
     StateIndependentNoiseModel,
     SinglePairConductanceConverter,
+    DualPairConductanceConverter,
+    NPairConductanceConverter,
+    CustomPairConductanceConverter,
 )
 
 from .helpers.testcases import AihwkitTestCase
@@ -41,6 +45,24 @@ class NoiseModelTest(AihwkitTestCase):
         weights = randn(10, 35)
 
         noise_model = StateIndependentNoiseModel()
+        t_inference = 100.0
+        noisy_weights = noise_model.apply_noise(weights, t_inference)
+
+        self.assertNotAlmostEqualTensor(noisy_weights, weights)
+
+    def test_apply_custom_drift(self):
+        """Test custom drift model with g_converter"""
+        weights = randn(10, 35)
+
+        g_min, g_max = 0., 25.
+        custom_drift_model = dict(g_lst=[g_min, 10., g_max],
+                                  nu_mean_lst=[0.08, 0.05, 0.03],
+                                  nu_std_lst=[0.03, 0.02, 0.01])
+
+        noise_model = CustomDriftPCMLikeNoiseModel(
+            custom_drift_model,
+            g_converter=SinglePairConductanceConverter(g_min=g_min, g_max=g_max),
+        )
         t_inference = 100.0
         noisy_weights = noise_model.apply_noise(weights, t_inference)
 
@@ -79,3 +101,115 @@ class ConductanceConverterTest(AihwkitTestCase):
         converted_weights = g_converter.convert_back_to_weights(g_lst, params)
 
         self.assertTensorAlmostEqual(weights, converted_weights)
+
+    def test_dual_pair_converter(self):
+        """Tests the dual pair converter."""
+        g_max = 3.123
+        g_min = 0.789
+
+        weights = randn(10, 35)
+
+        g_converter = DualPairConductanceConverter(f_lst=[1.0, 3.0],
+                                                   g_max=g_max,
+                                                   g_min=g_min)
+
+        g_lst, params = g_converter.convert_to_conductances(weights)
+
+        tolerance = 1e-6
+        for g_plus, g_minus in zip(g_lst[::2], g_lst[1::2]):
+
+            g_plus = g_lst[0].detach().cpu().numpy()
+            g_minus = g_lst[1].detach().cpu().numpy()
+
+            self.assertTrue(
+                (g_plus > g_max - tolerance).sum() + (g_minus > g_max - tolerance).sum() > 0
+            )
+            self.assertTrue(
+                (g_plus < g_min - tolerance).sum() + (g_minus < g_min - tolerance).sum() == 0
+            )
+            self.assertTrue(
+                (g_plus > g_max + tolerance).sum() + (g_minus > g_max + tolerance).sum() == 0
+            )
+
+        converted_weights = g_converter.convert_back_to_weights(g_lst, params)
+
+        self.assertTensorAlmostEqual(weights, converted_weights)
+
+    def test_n_pair_converter(self):
+        """Tests the dual pair converter."""
+        g_max = 3.123
+        g_min = 0.789
+
+        weights = randn(10, 35)
+
+        g_converter = NPairConductanceConverter(f_lst=[1.0, 2.0, 3.0],
+                                                g_max=g_max,
+                                                g_min=g_min)
+
+        g_lst, params = g_converter.convert_to_conductances(weights)
+
+        tolerance = 1e-6
+        for g_plus, g_minus in zip(g_lst[::2], g_lst[1::2]):
+
+            g_plus = g_lst[0].detach().cpu().numpy()
+            g_minus = g_lst[1].detach().cpu().numpy()
+
+            self.assertTrue(
+                (g_plus > g_max - tolerance).sum() + (g_minus > g_max - tolerance).sum() > 0
+            )
+            self.assertTrue(
+                (g_plus < g_min - tolerance).sum() + (g_minus < g_min - tolerance).sum() == 0
+            )
+            self.assertTrue(
+                (g_plus > g_max + tolerance).sum() + (g_minus > g_max + tolerance).sum() == 0
+            )
+
+        converted_weights = g_converter.convert_back_to_weights(g_lst, params)
+
+        self.assertTensorAlmostEqual(weights, converted_weights)
+
+    def test_custom_pair_converter(self):
+        """Tests the dual pair converter."""
+        g_max = 3.123
+        g_min = 0.789
+
+        weights = randn(10, 35)
+
+        g_converter = CustomPairConductanceConverter(f_lst=[1.0],
+                                                     g_lst=[[g_min,
+                                                             g_min,
+                                                             g_min,
+                                                             (g_max - g_min) / 2 + g_min,
+                                                             g_max],
+                                                            [g_max,
+                                                             (g_max - g_min) / 2 + g_min,
+                                                             g_min,
+                                                             g_min,
+                                                             g_min],
+                                                            ],
+                                                     g_min=g_min,
+                                                     g_max=g_max,
+                                                     invertibility_test=False,
+                                                     )
+
+        g_lst, params = g_converter.convert_to_conductances(weights)
+
+        tolerance = 1e-6
+        for g_plus, g_minus in zip(g_lst[::2], g_lst[1::2]):
+
+            g_plus = g_lst[0].detach().cpu().numpy()
+            g_minus = g_lst[1].detach().cpu().numpy()
+
+            self.assertTrue(
+                (g_plus > g_max - tolerance).sum() + (g_minus > g_max - tolerance).sum() > 0
+            )
+            self.assertTrue(
+                (g_plus < g_min - tolerance).sum() + (g_minus < g_min - tolerance).sum() == 0
+            )
+            self.assertTrue(
+                (g_plus > g_max + tolerance).sum() + (g_minus > g_max + tolerance).sum() == 0
+            )
+
+        converted_weights = g_converter.convert_back_to_weights(g_lst, params)
+
+        self.assertTensorAlmostEqual(weights, converted_weights)    # invertibility test
